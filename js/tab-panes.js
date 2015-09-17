6232c4c3b95004d5cb9f5cd8e9624ae78ddb22ec
shioyang <shioyang4321@gmail.com>
2015-09-17 22:50:36 +0900

    Implement removing a selected URL

--- js/tab-panes.js
+++ js/tab-panes.js
@@ -11,9 +11,26 @@ angular.module('tab-panes', [])
 					{ url: 'http://evel03.url/' }
 				];
 
+				this.selectedUrl = null;
+
 				this.addUrl = function() {
 					console.log(this.addedUrl);
+					this.urlInfos.push({ url: this.addedUrl });
 					this.addedUrl = ""; //clear
+				};
+
+				this.setSelected = function(url) {
+					this.selectedUrl = url;
+				};
+
+				this.removeSelectedUrl = function() {
+					if (this.selectedUrl != null) {
+						var index = this.urlInfos.indexOf(this.selectedUrl);
+						if (0 <= index) {
+							this.urlInfos.splice(index, 1);
+							this.selectedUrl = null;
+						}
+					}
 				};
 			},
 			controllerAs: 'listPane'
