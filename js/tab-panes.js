fd094e89264e8cc4b1759ce71a66a7dc5f152ef9
shioyang <shioyang4321@gmail.com>
2015-09-17 12:53:02 +0900

    Fix typo "controllerAs"

--- js/tab-panes.js
+++ js/tab-panes.js
@@ -6,11 +6,11 @@ angular.module('tab-panes', [])
 			templateUrl: 'js/list-pane.html',
 			controller: function() {
 				this.urlInfos = [
-					{url: "http://evel01.url/"},
-					{url: "http://evel02.url/"},
-					{url: "http://evel03.url/"}
+					{ url: 'http://evel01.url/' },
+					{ url: 'http://evel02.url/' },
+					{ url: 'http://evel03.url/' }
 				];
 			},
-			cntrollerAs: 'listPane'
+			controllerAs: 'listPane'
 		};
 	})
