af3fd34cc1a810c33ce9e5da3c75addcb78fcdde
shioyang <shioyang4321@gmail.com>
2015-09-17 22:55:15 +0900

    Remove test codes

--- js/main.js
+++ js/main.js
@@ -1,11 +1,6 @@
 (function() {
 	var app = angular.module("popupApp", ["components"]);
 
-	app.controller("TestClickCtrl", function() {
-		this.click = function() {
-			alert("Clicked!");
-		};
-	});
 })();
 
 
