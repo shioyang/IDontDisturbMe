f7a5a5e3a8962ee0c4614727e7903e6f13faaaf8
shioyang <shioyang4321@gmail.com>
2015-10-01 09:46:59 +0900

    Update testSpec

--- ut/testSpec.js
+++ ut/testSpec.js
@@ -1,5 +1,8 @@
-describe('aaa', function() {
-	it('Hello UT', function() {
+describe('Hello UT', function() {
+	it('To be success', function() {
 		expect(1).toBe(1);
+	});
+	it('To be fail', function() {
+		expect(0).toBe(1);
 	});
 });
